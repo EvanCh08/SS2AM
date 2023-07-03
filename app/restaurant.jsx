1377b173bbe8001dd7fe653d6c7b1d2cd7270a11
EvanCh08 <122248689+EvanCh08@users.noreply.github.com>
2023-07-03 12:32:00 +0700

    Update restaurant.jsx

--- app/restaurant.jsx
+++ app/restaurant.jsx
@@ -11,10 +11,14 @@ export default function Restaurant() {
     const [price, setPrice] = useState('');
     const [error, setError] = useState(null);
     const [id, setId] = useState('');
+    const [data, setData] = useState([]);
+    const [temp, setTemp] = useState([]);
     const [allIds, setAllIds] = useState([]);
     const [allInputs, setAllInputs] = useState([]);
-    const [uniqueId, setUniqueId] = useState('');
+    const [uniqueId, setUniqueId] = useState('')
     const counter = 0;
+
+
 
     const handleSubmit = async () => {
         if (menu == '' || price == '' || uniqueId == '') {
@@ -25,7 +29,7 @@ export default function Restaurant() {
         const { data, error } = await supabase
             .from('menu')
             .insert([{ menu, price, uniqueId, counter}]);
-
+        
         await AsyncStorage.setItem('viewid', uniqueId)
 
         if (error) {
@@ -34,8 +38,60 @@ export default function Restaurant() {
         } else {
             setError(null);
         }
+
     }
 
+    const handleInput = () => { //jangan diapus
+        const theData = {
+            menu: menu, 
+            price: price,
+            counter: 0
+        };
+        
+        const i = allIds.findIndex(lol => lol == id);
+        if (i >= 0) {
+            console.log(1)
+            setAllInputs([...allInputs, theData])
+            console.log(allInputs)
+        } else {
+            setAllInputs([theData])
+            setAllIds([...allIds, id])
+            console.log(allIds)
+            console.log(allInputs)
+        }
+    }
+
+    const submitData = async () => { //jangan diapus
+        try {
+            await AsyncStorage.setItem(id, JSON.stringify(allInputs))
+            await AsyncStorage.setItem('viewid', id)
+            console.log(id)
+        } catch (error) {
+            setError(error)
+        }
+    }
+
+    /*const retrieveData = async () => {
+        try {
+            const final = JSON.parse(await AsyncStorage.getItem(id))
+            console.log(final)
+        } catch (error) {
+            setError(error)
+        }
+    }
+    */
+
+    //jangan lupa balikin onChangeText jadi setId lagi
+
+    /*Button //jangan diapus
+                onPress={handleInput} 
+                mode="contained" buttonColor="#394d46" style={{marginTop: 20}}>Submit</Button>
+    <Button onPress={submitData} 
+                    mode="contained" 
+                    buttonColor="#394d46"
+                    style={{marginTop: 10, marginBottom: 15}}
+                    >Save Data</Button>
+    */
     
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#e4e7d1"}}>
@@ -61,7 +117,7 @@ export default function Restaurant() {
                 backgroundColor="#e4e7d1"
                 activeUnderlineColor="#394d46"
                 />
-            <Button mode='contained' style={{marginBottom:  15, marginTop: 15}} buttonColor='#394d46' onPress={handleSubmit}>Submit</Button>
+            <Button mode='contained' style={{marginBottom:  15, marginTop: 20}} buttonColor='#394d46' onPress={handleSubmit}>Submit</Button>
             <Link href="/Viewmenus">
                 <Button mode="contained" 
                 buttonColor="#394d46" 
