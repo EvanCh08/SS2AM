425203d6b2e7f034f6b388ac79935f2b1f3e839b
EvanCh08 <122248689+EvanCh08@users.noreply.github.com>
2023-07-03 12:32:25 +0700

    Update splitandcombine.jsx

--- app/splitandcombine.jsx
+++ app/splitandcombine.jsx
@@ -1,21 +1,35 @@
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import {  Button } from 'react-native-paper';
 import { Link } from "expo-router";
 
 export default function Split() {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#e4e7d1"}}>
-            <Link href="/allcustomers">
-                <Button mode="contained" buttonColor="#394d46">
-                    Split
-                </Button>
-            </Link>
+            <Image 
+                style={{height: 60, width: 250, marginBottom: 40}}
+                source={{uri: "/Users/evandarrenchristanto/Downloads/splitrr.jpeg"}}/>
 
-            <Link href="/combinebill">
-                <Button mode="contained" buttonColor="#394d46">
-                    Combine
-                </Button>
-            </Link>
+            <View style={{marginBottom: 5}}>
+                <Link href="/allcustomers">
+                    <Button mode="contained" buttonColor="#394d46">
+                        Split Bill
+                    </Button>
+                </Link>
+            </View>
+            
+            <View style={{marginTop: 5}}>
+                <Link href="/combinebill">
+                    <Button mode="contained" buttonColor="#394d46" >
+                        Combine
+                    </Button>
+                </Link>
+            </View>
+            <View style={{marginTop: 10}}>
+
+                <Link href="/customer">
+                    <Button  mode="contained" buttonColor='#394d46'>Back</Button>
+                </Link>
+            </View>
         </View>
     )
 }
