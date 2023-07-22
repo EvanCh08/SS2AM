578c28b92fefed8141d31a34feef92af5f83b4aa
EvanCh08 <122248689+EvanCh08@users.noreply.github.com>
2023-07-22 19:02:00 +0700

    Update insertunique.jsx

--- app/insertunique.jsx
+++ app/insertunique.jsx
@@ -85,7 +85,6 @@ export default function Restaurant() {
                 return;
             }
         } catch (error) {
-            // Handle the error
             console.log(error);
         }
     };
